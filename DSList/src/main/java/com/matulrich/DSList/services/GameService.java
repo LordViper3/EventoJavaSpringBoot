e34a8c47b0105c3d30c8ce67df28ea25c0366fa0
soapbody <mateus_teti98@hotmail.com>
2023-05-10 19:43:45 -0300

    GameDTO created

--- DSList/src/main/java/com/matulrich/DSList/services/GameService.java
+++ DSList/src/main/java/com/matulrich/DSList/services/GameService.java
@@ -1,9 +1,11 @@
 package com.matulrich.DSList.services;
+import com.matulrich.DSList.dto.GameDTO;
 import com.matulrich.DSList.dto.GameMinDTO;
 import com.matulrich.DSList.entities.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.matulrich.DSList.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +14,13 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
