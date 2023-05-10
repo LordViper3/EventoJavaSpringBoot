8feb64d270427a9bd0208de49bfe52271c251376
soapbody <mateus_teti98@hotmail.com>
2023-05-09 23:44:40 -0300

    firstCommit

---
+++ DSList/src/test/java/com/matulrich/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matulrich.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
